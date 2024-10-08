f5f7e8aa29e3e3897486ef77139af5c6f10c9492
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-10-08 10:17:04 +0200

    adding some beers as data

--- app/src/main/java/com/hfad/beeradvisor/MainActivity.kt
+++ app/src/main/java/com/hfad/beeradvisor/MainActivity.kt
@@ -16,7 +16,18 @@ class MainActivity: AppCompatActivity() {
             val beerColor = findViewById<Spinner>(R.id.beer_color)
             val color = beerColor.selectedItem
             val brands = findViewById<TextView>(R.id.brands)
-            brands.text = "Beer color is $color"
+            val beerList = getBeers(color.toString())
+            val beers = beerList.reduce { str, item -> str + "\n" + item }
+            brands.text = beers
         }
+    }
+}
+
+fun getBeers(color: String): List<String>{
+    return when (color) {
+        "Light" -> listOf("Jail Pale Ale", "Lager Light")
+        "Amber" -> listOf("Jack Amber", "Red Moose")
+        "Brown" -> listOf("Brown Bear Beer", "Bock Brownie")
+        else -> listOf("Gout Stout", "Dark Daniel")
     }
 }
